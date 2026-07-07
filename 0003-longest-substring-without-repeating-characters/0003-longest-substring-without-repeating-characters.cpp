594625b981f4be8e3e3aca4ed4b6f649bd2964e9
Pogi-hub <pogihub@gmail.com>
2026-07-07 17:19:01 +0530

    Time: 8 ms (56.46%), Space: 11.8 MB (55.99%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int mx=0; int j=0; int ans=0;
+        int mx = 0;
+        int j = 0;
+        int ans = 0;
 
-        unordered_map<int,int> mp;
-        for(int i=0;i<s.size();i++){
-            if(mp.find(s[i])==mp.end()){
-                mp[s[i]]=i;
-                mx++;
+        unordered_map<char,int> mp;
+
+        for (int i = 0; i < s.size(); i++) {
+
+            if (mp.find(s[i]) != mp.end()) {
+                j=max(j,mp[s[i]]+1);
             }
-            else{
-                i=mp[s[i]];
-                ans=max(ans,mx);
-                mx=0;
-                mp.clear();
-            }
+
+            mp[s[i]]=i;
+
+            mx=i-j+1;
+
             ans=max(ans,mx);
         }
 
