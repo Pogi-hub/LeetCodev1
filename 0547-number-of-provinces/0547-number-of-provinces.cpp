4de83f7970a19f7644b7b872e437bcee6c31c80e
Pogi-hub <pogihub@gmail.com>
2026-07-16 15:10:51 +0530

    Time: 4 ms (24.71%), Space: 19.4 MB (83.27%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
-    void dfs(int node,vector<int> &vis,vector<vector<int>> &adj){
-        vis[node]=1;
+    void dfs(int node, vector<int>& vis, vector<vector<int>>& isConnected) {
+    vis[node] = 1;
 
-        for(auto it: adj[node]){
-            if(!vis[it]){
-                dfs(it,vis,adj);
+        for (int j = 0; j < isConnected.size(); j++) {
+            if (isConnected[node][j] && !vis[j]) {
+            dfs(j, vis, isConnected);
             }
         }
     }
+
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
         vector<int> vis(n,0);
         int count=0;
 
-        vector<vector<int>> adj(n);
+        // vector<vector<int>> adj(n);
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1)
-                    adj[i].push_back(j);
-            }
-        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(isConnected[i][j]==1)
+        //             adj[i].push_back(j);
+        //     }
+        // }
 
         for(int i=0;i<n;i++){
             if(!vis[i]) {
-                dfs(i,vis,adj);
+                dfs(i,vis,isConnected);
                 count++;
             }
         }
