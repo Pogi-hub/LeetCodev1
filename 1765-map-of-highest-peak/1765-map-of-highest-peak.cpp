a7ca22269deb5c7f0559ea9e66fb72c832fd1cc9
Pogi-hub <pogihub@gmail.com>
2026-07-29 17:45:38 +0530

    Time: 74 ms (72.94%), Space: 112.9 MB (76.66%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        int m=isWater.size();
+        int n=isWater[0].size();
+
+        vector<vector<int>> ans(m,vector<int> (n,-1));
+        queue<pair<int,int >> q;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(isWater[i][j]==1) { 
+                    ans[i][j]=0;
+                    q.push({i,j});
+                }
+            }
+        }
+
+        int dx[4]={1,-1,0,0};
+        int dy[4]={0,0,1,-1};
+
+        while(!q.empty()){
+            int x=q.front().first;
+            int y=q.front().second;
+
+            q.pop();
+
+            for(int i=0;i<4;i++){
+                int nx=dx[i]+x;
+                int ny=dy[i]+y;
+
+                if(nx>=0 && ny>=0 && nx<m && ny<n && ans[nx][ny]==-1){
+                    ans[nx][ny]=ans[x][y]+1;
+                    q.push({nx,ny});
+                }
+            }
+        }
+
+        return ans;
+    }
+};
