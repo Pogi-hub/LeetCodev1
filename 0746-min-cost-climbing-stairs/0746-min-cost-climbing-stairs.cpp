5c1184857eaa7d9dd0eac1053d8d3545689dff96
Pogi-hub <pogihub@gmail.com>
2026-08-08 22:14:13 +0530

    Time: 0 ms (100%), Space: 17.4 MB (89.53%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int> dp (n+1,-1);
-        dp[0]=dp[1]=0;
+        int prev1=0,prev2=0;
+        int curr=0;
 
         for(int i=2;i<=n;i++){
-            dp[i]=min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+            curr=min(prev1+cost[i-2],prev2+cost[i-1]);
+            prev1=prev2;
+            prev2=curr;
         }
 
-        return dp[n];
+        return curr;
         
     }
 };
