d0b16b7997312a500543a6e1dd02cdd2bc71445d
Pogi-hub <pogihub@gmail.com>
2026-08-10 11:22:36 +0530

    Time: 43 ms (43.76%), Space: 19 MB (60.44%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int recurse2(vector<int> &coins,int sum,int amt,vector<int> &dp){
+        if(sum==amt) return 0;
+
+        if(sum>amt) return 1e9;
+        int ans=1e9;
+        
+        if(dp[sum]!=-1) return dp[sum];
+
+        for(int i=0;i<coins.size();i++){
+            if (coins[i] <= amt - sum) 
+            ans = min(ans, 1 + recurse2(coins, sum + coins[i], amt, dp));
+        }
+
+        return dp[sum]=ans;
+    }
+    int recurse1(vector<int> &coins,int i,int sum,int amt){
+        if(sum==amt) return 0;
+        if(i==coins.size()) return 1e9;
+
+        int notpick=recurse1(coins,i+1,sum,amt);
+
+        int pick=1e9;
+        if(coins[i]+sum<=amt) pick=1+recurse1(coins,i,sum+coins[i],amt);
+
+        return min(pick,notpick);
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        // if(amount==0) return 0;
+        // int ans=recurse1(coins,0,0,amount);
+        // if(ans==1e9) return -1;
+        // return ans;
+
+        //recurse 2 approach
+        vector<int> dp (amount+1,-1);
+        if(amount==0) return 0;
+        int ans=recurse2(coins,0,amount,dp);
+        if(ans==1e9) return -1;
+        return ans;
+    //     int n=nums.size();
+        
+
+    //     for(int i=0;i<n;i++){
+    //         for(int sum=0;sum<=amount;sum++){
+
+    //         }
+    //     }
+        
+    }
+};
