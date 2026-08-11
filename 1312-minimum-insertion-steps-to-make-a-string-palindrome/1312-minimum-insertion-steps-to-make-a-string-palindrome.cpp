70316df88bdc955fcf784052a83fae2b84b5c121
Pogi-hub <pogihub@gmail.com>
2026-08-11 23:17:43 +0530

    Time: 23 ms (77.96%), Space: 9.8 MB (86.11%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int len=s.size();
+        string text1=s;
+        string text2=s;
+        reverse(text2.begin(),text2.end());
+
+        vector<int> prev(len,0);
+        vector<int> curr(len,0);
+
+        //initialising prev array dp
+        for(int j=0; j<len; j++) {
+            if(text2[j]==text1[0])
+                prev[j]=1;
+
+            if(j>0)
+                prev[j] = max(prev[j],prev[j-1]);
+        }
+
+        for(int i=1;i<len;i++){
+            for(int j=0;j<len;j++){
+                if(text1[i]==text2[j]){
+                curr[j]=(j>0 ? prev[j-1]:0) + 1;
+                } else {
+                curr[j]=max(prev[j], j>0?curr[j-1] : 0);
+                }
+            }
+            prev=curr;
+        }
+
+        return len-prev[len-1];
+    }
+};
