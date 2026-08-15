6a443476e2e5702fe9e0827f800bf924e9e744d3
Pogi-hub <pogihub@gmail.com>
2026-08-15 15:25:33 +0530

    Time: 20 ms (23.33%), Space: 27.1 MB (42.75%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -11,38 +11,23 @@
  */
 class Solution {
 public:
+    vector<TreeNode*> ans;
+    void dfs(TreeNode* &root, unordered_set<int> &st, bool isRoot){
+        if(!root) return;
+
+        bool deleted=st.count(root->val);
+
+        if(isRoot && !deleted) ans.push_back(root);
+
+        dfs(root->left,st,deleted);
+        dfs(root->right,st,deleted);
+
+        if(deleted) root=nullptr;
+        return;
+    }
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
         unordered_set<int> st(to_delete.begin(),to_delete.end());
-        vector<TreeNode*> ans;
-        if(!root) return ans;
-
-        queue<pair<TreeNode*,bool>> q;
-        q.push({root,true});
-
-        while(!q.empty()){
-            TreeNode* node=q.front().first;
-            bool parent_deleted=q.front().second;
-            q.pop();
-
-            bool deleted=st.count(node->val);
-
-            if(parent_deleted && !deleted)  ans.push_back(node);
-
-            if(node->left){
-                bool childDeleted = st.count(node->left->val);
-                q.push({node->left, deleted});
-                if(childDeleted) node->left=nullptr;
-            }
-
-            if(node->right){
-                bool childDeleted = st.count(node->right->val);
-                q.push({node->right, deleted});
-                if(childDeleted) node->right=nullptr;
-            }
-
-            if(deleted) delete node;
-        }
-
-        return ans;
+        dfs(root,st,true); 
+        return ans; 
     }
 };
