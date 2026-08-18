1fafb281e46d7564329ff8c5730957fe986af428
Pogi-hub <pogihub@gmail.com>
2026-08-19 04:27:08 +0530

    Time: 19 ms (96.18%), Space: 14.4 MB (99.13%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-    using p=pair<int,int>;
-    struct pairHash {
-        size_t operator()(const p& a) const {
-            return hash<int>()(a.first) ^ (hash<int>()(a.second) << 1);
-        }
-    };
     int minCostConnectPoints(vector<vector<int>>& points) {
-        int n=points.size();
-        int sum=0;
-        unordered_map<p, vector<p>, pairHash> adj;
-        for(int i=0;i<n;i++){
-            int x1=points[i][0];
-            int y1=points[i][1];
-            for(int j=i+1;j<n;j++){
-                int x2=points[j][0];
-                int y2=points[j][1];
-                adj[{x1,y1}].push_back({x2,y2});
-                adj[{x2,y2}].push_back({x1,y1});
+
+        int n = points.size();
+
+        vector<int> minDist(n, INT_MAX);
+        vector<bool> vis(n, false);
+
+        minDist[0] = 0;
+
+        int sum = 0;
+
+        for(int count = 0; count < n; count++) {
+
+            // Find unvisited point with minimum connection cost
+            int u = -1;
+
+            for(int i = 0; i < n; i++) {
+                if(!vis[i] && (u == -1 || minDist[i] < minDist[u])) {
+                    u = i;
+                }
             }
-        }
-        set<pair<int,int>> vis;
-        set<pair<int,p>> st;
-        st.insert({0,{points[0][0],points[0][1]}});
 
-        while(!st.empty()){
-            auto parent=*(st.begin());
-            int p_wt=parent.first;
-            int px=parent.second.first;
-            int py=parent.second.second;
-            st.erase(parent);
+            // Add this point to MST
+            vis[u] = true;
+            sum += minDist[u];
 
-            if(vis.count({px,py})) continue;
-            vis.insert({px,py});
-            sum+=p_wt;
+            // Update distances of remaining points
+            for(int v = 0; v < n; v++) {
 
-            for(auto &[cx,cy]:adj[{px,py}]){
-                int wt=abs(px-cx)+abs(py-cy);
-                st.insert({wt,{cx,cy}});
+                if(!vis[v]) {
+
+                    int wt = abs(points[u][0] - points[v][0]) +
+                             abs(points[u][1] - points[v][1]);
+
+                    minDist[v] = min(minDist[v], wt);
+                }
             }
         }
 
