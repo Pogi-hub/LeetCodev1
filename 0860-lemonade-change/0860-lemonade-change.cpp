d94de2f8b7b62c9cb712eeb249f57e53ac4913b8
Pogi-hub <pogihub@gmail.com>
2026-08-21 00:37:56 +0530

    Time: 0 ms (100%), Space: 87.3 MB (11.27%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n=bills.size();
+        unordered_map<int,int> mp;
+        mp[5]=0;
+        mp[10]=0;
+        for(int i=0;i<n;i++){
+            if(bills[i]==20){
+                if(mp[10]>=1 && mp[5]>=1){
+                    mp[10]--; mp[5]--;
+                }
+                else if(mp[5]>=3){
+                    mp[5]-=3;
+                }
+                else return false;
+            }
+            else if (bills[i]==10){
+                if(mp[5]>=1) {
+                    mp[5]--;
+                    mp[10]++;
+                }
+                else return false;
+            }
+            else {
+                mp[5]++;
+            }
+        }
+        return true;
+    }
+};
