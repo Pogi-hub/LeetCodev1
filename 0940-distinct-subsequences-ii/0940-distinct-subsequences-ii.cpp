35ec31ff91e8e947ed8ecb94cc5ea7c27f04eb1f
Pogi-hub <pogihub@gmail.com>
2026-09-08 01:53:19 +0530

    Time: 0 ms (100%), Space: 8.9 MB (62.55%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.cpp
@@ -3,17 +3,19 @@ int MOD=1e9+7;
 public:
     int distinctSubseqII(string s) {
         int n=s.size();
+        vector<int> endWith(26,0);
 
-        vector<int> dp(n,1);
-        int res=0;
+        int sum=0;
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(s[i]==s[j]) continue;
-                dp[i]=(dp[i]+dp[j])%MOD;
-            }
-            res=(res+dp[i])%MOD;
+            int idx=s[i]-'a';
+
+            int curr=(1+sum-endWith[idx]+MOD)%MOD;
+            sum=(sum+curr)%MOD;
+
+            endWith[idx]=(endWith[idx]+curr)%MOD;
         }
-        return res;
+
+        return sum;
     }
 };
