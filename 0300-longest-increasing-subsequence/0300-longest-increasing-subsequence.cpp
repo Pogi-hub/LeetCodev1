4dbc305d100f38bea109598a79cfc74f67b04484
Pogi-hub <pogihub@gmail.com>
2026-09-08 06:37:05 +0530

    Time: 3 ms (79.11%), Space: 13.9 MB (99.45%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -13,17 +13,29 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        for(int i=n-1;i>=0;i--){
-            for(int prev=i-1;prev>=-1;prev--){
-                int len=dp[i+1][prev+1];
-                if(prev==-1||nums[i]>nums[prev]){
-                    len=max(len,1+dp[i+1][i+1]);
-                }
-                dp[i][prev+1]=len;
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        // for(int i=n-1;i>=0;i--){
+        //     for(int prev=i-1;prev>=-1;prev--){
+        //         int len=dp[i+1][prev+1];
+        //         if(prev==-1||nums[i]>nums[prev]){
+        //             len=max(len,1+dp[i+1][i+1]);
+        //         }
+        //         dp[i][prev+1]=len;
 
+        //     }
+        // }
+        // return dp[0][0];
+        vector<int> temp;
+        temp.push_back(nums[0]);
+
+        for(int i=1;i<n;i++){
+            if(nums[i]>temp.back()) temp.push_back(nums[i]);
+            else {
+                int idx=lower_bound(temp.begin(),temp.end(),nums[i])
+                        -temp.begin();
+                temp[idx]=nums[i];
             }
         }
-        return dp[0][0];
+        return temp.size();
     }
 };
